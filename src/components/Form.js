bb763ec1312dffc8ad45174002f166d050f57eb1
shantelraya <parischax@gmail.com>
2022-09-26 20:52:44 +0300

    done assignments

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 
 function Form(props) {
-  const [firstName, setFirstName] = useState("Sylvia");
-  const [lastName, setLastName] = useState("Woods");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
 
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+  
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
   }
@@ -13,7 +17,7 @@ function Form(props) {
   }
 
   return (
-    <form>
+    <form onSubmit={handlesubmit}>
       <input type="text" onChange={handleFirstNameChange} value={firstName} />
       <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
